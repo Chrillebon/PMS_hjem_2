eba862d8c6bcf2dce765c9a5ace043bf22ad5da3
MEGAjosni <jonassoeborgnielsen@gmail.com>
2020-11-12 16:13:56 +0100

    Update main.c

--- part1/main.c
+++ part1/main.c
@@ -8,17 +8,16 @@ int main(void) {
     matrix_t * A = read_matrix("A.txt");
     vector_t * b = read_vector("b.txt");
 
-    vector_t * x = b;
+    print_matrix(A);
+    printf("*x = \n");
+    print_vector(b);
 
     int info = call_dgesv(A,b);
 
     /* Present solution */
     if (info == 0)
     {
-        print_matrix(A);
-        printf("*");
-        print_vector(x);
-        printf("=");
+        printf("Solution is: x = \n");
         print_vector(b);
     } else {
         printf("No solution was found!\n");
